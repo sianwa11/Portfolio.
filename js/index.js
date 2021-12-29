642d172127f2a136fd299e6f88cf6b2ec16ac8cf
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2021-12-29 09:18:39 +0300

    Animation on intersection observer works

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
 "use strict";
 
 const timeline = document.querySelectorAll(".timeline");
-// timeline.forEach((el) => {
-//   el.style.opacity = 0;
-// });
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      //   entry.target.classList.toggle("animation-left", entry.isIntersecting);
-
-      console.log(entry);
-
       if (entry.isIntersecting) {
+        // make clear
         entry.target.style.opacity = 1;
+        // animate left timeline
+        entry.target.children["timeline-left"].classList.add("animation-left");
+        // animate right timeline
+        entry.target.children["timeline-right"].classList.add(
+          "animation-right"
+        );
+        // animate center timeline
+        entry.target.children["timeline-center"].classList.add(
+          "animation-bottom"
+        );
+        // unobserve
         observer.unobserve(entry.target);
       }
     });
